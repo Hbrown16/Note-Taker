40ea3c9b19a55a6d4c3a4a87012a93421c2a8b40
Hbrown16 <hbrown1094@gmail.com>
2021-07-19 17:50:09 -0400

    connecting routes

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ const PORT = process.env.PORT || 8080;
 // Create Express APP
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+// serves files from directory
+app.use(express.static("public"));
+
+
+app.use("/", require("./Routes/HTMLroutes"));
+app.use("/api", require("./Routes/apiroutes"));
