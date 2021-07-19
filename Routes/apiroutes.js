69ef8a0bae6efab6923302377e60dd9f24942e02
Hbrown16 <hbrown1094@gmail.com>
2021-07-19 16:53:21 -0400

    new note function

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -1,8 +1,25 @@
 const { Router } = require('express').Router()
 const fs = require('fs')
 
-let nates = require('')
+let nates = require('../db.json')
 
 // Get current notes
+Router.get('/notes', (req, res) => {
+    res.json(notes)
+})
 
 
+// This is what will allow you to create a note
+Router.post('/notes', (req, res) => {
+    var newNote = req.body
+    if (notes.length) {
+        newNote.id = notes[notes.length -1].id + 1
+    }else{
+        newNote.id = 1
+    }
+    notes.push(newNote)
+    fs.writeFile('../db.json', JSON.stringify(notes), err => {
+        console.log(err)
+        res.sendStatus(200)
+    })
+})
