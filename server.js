57e1c4942feba46d545c0564ba890e69aaaa6458
Hbrown16 <hbrown1094@gmail.com>
2021-07-19 17:45:42 -0400

    express

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const express = require("express")
 // Creating port connection
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Create Express APP
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
