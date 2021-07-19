7d53596c57c7a3754c291bd8d08b1bc387019bc4
Hbrown16 <hbrown1094@gmail.com>
2021-07-19 17:20:00 -0400

    html fun

--- Routes/HTMLroutes.js
+++ Routes/HTMLroutes.js
@@ -1,1 +1,7 @@
 // Routes HTML
+const router = require ('express').Router()
+const path = require ('path');
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(_dirname, "./public/assets/notes.html"))
+});
