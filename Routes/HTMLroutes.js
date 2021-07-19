1bfc900932af1bc78dd029ab59ca3760fbad3e7d
Hbrown16 <hbrown1094@gmail.com>
2021-07-19 17:23:28 -0400

    html routes done

--- Routes/HTMLroutes.js
+++ Routes/HTMLroutes.js
@@ -5,3 +5,8 @@ const path = require ('path');
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(_dirname, "./public/assets/notes.html"))
 });
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, './public/notes.html'))
+})
+module.exports = router;
